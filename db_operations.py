87f084b4a08cf6191f94ca8f23c068c374dff8c5
dima <kharitonovdo@yande.ru>
2025-02-06 13:26:22 +0300

    apst

--- db_operations.py
+++ db_operations.py
@@ -6,7 +6,8 @@ cursor = connection.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS DANNIE (
     id INTEGER PRIMARY KEY,
-    username TEXT
+    username TEXT,
+    galka INTEGER
     )
 ''') 
 connection.commit()
@@ -16,7 +17,7 @@ connection.close()
 def added(text):
     connection = sqlite3.connect('to_do_list.db')
     cursor = connection.cursor()
-    cursor.execute('INSERT INTO DANNIE (username) VALUES(?)', (text, ))
+    cursor.execute('INSERT INTO DANNIE (username, galka) VALUES (?, ?)', (text, 0))
     connection.commit()
     connection.close()
 
@@ -24,5 +25,20 @@ def delete(id):
     connection = sqlite3.connect('to_do_list.db')
     cursor = connection.cursor()
     cursor.execute('DELETE FROM DANNIE WHERE id=?', (id,)) 
+    connection.commit()
+    connection.close()
+
+def get_text():
+    connection = sqlite3.connect('to_do_list.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM DANNIE')
+    text = cursor.fetchall()
+    connection.close()
+    return text
+
+def galka(id, bolev):
+    connection = sqlite3.connect('to_do_list.db')
+    cursor = connection.cursor()
+    cursor.execute('UPDATE DANNIE SET galka=? WHERE id=?', (bolev, id,))
     connection.commit()
     connection.close()
--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import db_operations
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'get'])
 def index():
+    text = db_operations.get_text()
+    if text is None:
+        text=[]
     if request.method == 'POST':
         text = request.form.get('text')
-        db_operations.added(text)
-        print(text)
-        return render_template('index.html', text=text)
-    return render_template('index.html')
+        text = db_operations.added(text)
 
+        return render_template('index.html', text=text, prod=db_operations.get_text())
+
+    return render_template('index.html', prod=text)
+
+@app.route('/delete/<int:id>', methods=['POST', 'GET'])
+def delete(id):
+    delete = db_operations.delete(id)
+    return redirect('/')
+
+@app.route('/galka/<int:id>/<int:bolev>', methods=['POST', 'GET'])
+def galka(id, bolev):
+    db_operations.galka(id, bolev)
+    return redirect('/')
 app.run()
